0e35ec199f3adcd080782a68352bfc7222fce023
jeandedieu <jeandedieusessou@gmail.com>
2024-07-05 18:56:45 +0000

    add getHashPassword function add getToken function

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 
-console.log(bcrypt);
-console.log(jwt);
+const getHashPassword = async (password) => {
+  let hashPassord;
+
+  try {
+    hashPassord = await bcrypt.hash(password, 10);
+    return hashPassord;
+  } catch (err) {
+    return new Error("password required");
+  }
+};
+
+const getToken = async (userId, hashPassord, inputPassword, secretKey) => {
+  try {
+    const result = await bcrypt.compare(inputPassword, hashPassord);
+
+    if (result) {
+      const token = jwt.sign({ ID: userId }, secretKey, { expiresIn: "30m" });
+
+      return token;
+    }
+  } catch (err) {
+    return err;
+  }
+};
+
+const authorization = (req, res, next) => {};
