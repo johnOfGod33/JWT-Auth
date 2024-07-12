2b2401556e15c2c36e076a81ec787a17247128a6
jeandedieu <jeandedieusessou@gmail.com>
2024-07-12 15:31:32 +0000

    fix verifyToken

--- index.js
+++ index.js
@@ -53,8 +53,7 @@ const authentification = async (
 const verifyToken = (secretKey) => {
   return (req, res, next) => {
     try {
-      console.log(req.headers.Authorization);
-      const token = req.headers.Authorization.split(" ")[1];
+      const token = req.headers["authorization"].split(" ")[1];
       const decodedToken = jwt.verify(token, secretKey);
       req.ID = decodedToken.ID;
       next();
